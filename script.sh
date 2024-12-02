58c0e8de614edea7f45ffb6d24fe56f060a829ec
Ryou Jae Hyung <rjh926@gmail.com>
2024-12-02 14:58:22 +0900

    script:

--- script.sh
+++ script.sh
@@ -1,12 +1,24 @@
+/usr/lib/ocf/resource.d/connect/ebs
+/usr/lib/ocf/resource.d/connect/vip
+/usr/lib/ocf/resource.d/connect/karaf
+/usr/lib/ocf/resource.d/connect/mariadb
+/usr/lib/ocf/resource.d/connect/coord
+
+chmod +x /usr/lib/ocf/resource.d/connect/*
+
 pcs resource create ebs ocf:connect:ebs volume_id=${VOLMUME_ID} --disabled
-pcs resource create vip ocf:connect:vip secondary_private_ip=${SECONDARY_PRIVATE_IP} netmask=${NATMASK} --disable
+pcs resource create vip ocf:connect:vip secondary_private_ip=${SECONDARY_PRIVATE_IP} netmask=${NATMASK} --disabled
 pcs constraint order start ebs then vip
 pcs constraint colocation add vip with ebs
 
 pcs resource create karaf ocf:connect:karaf --disabled
 pcs constraint order start vip then karaf
-pcs constraint colocation karaf with ebs
+pcs constraint colocation add karaf with ebs
 
 pcs resource create mariadb ocf:connect:mariadb --disabled
 pcs constraint order start vip then mariadb
-pcs constraint colocation mariadb with ebs
+pcs constraint colocation add mariadb with ebs
+
+pcs resource create coord ocf:connect:coord --disabled
+pcs constraint order start vip then coord
+pcs constraint colocation add coord with ebs
